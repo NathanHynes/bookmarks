7c533f14ffc4e1333e6e8a17d5179c67dfde028d
Nathan Hynes <47602619+NathanHynes@users.noreply.github.com>
2019-10-08 10:02:03 +0100

    Update bookmark_manager.rb
    updated database connection

--- lib/bookmark_manager.rb
+++ lib/bookmark_manager.rb
@@ -4,7 +4,7 @@ class BookmarkManager
   attr_reader :bookmarks
 
   def self.all
-    con = PG.connect :dbname => 'bookmark_manager', :user => 'duncanskinner'
+    con = PG.connect :dbname => 'bookmark_manager', :user => 'student'
 
     rs = con.exec "SELECT * FROM bookmarks"
 
